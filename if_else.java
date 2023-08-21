d495c5ce8f42f2ec96c18c339775df5bdc3bda42
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-08-21 21:02:12 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,25 @@
+class if_else {
+    public static void main(String[] args){
+        int number = 10;
+        
+        if (number < 0){
+            System.out.println("the number is negative");
+        }
+        else if (number > 0){
+            System.out.println("the number is positive");
+        }
+        else {
+            System.out.println("the number is 0");
+        }
+
+        String language = "Java";
+        if (language=="Java"){
+            System.out.println("Best programming language");
+        }
+
+
+    }
+
+    
+}
+
