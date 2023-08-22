37d79a9bf6b695f10c80bc601ad1382b9267e55a
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-08-22 20:02:00 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,10 @@
+class for_loop {
+    public static void main(String[] args){
+        int n = 5;
+        //for loop
+        for (int i=0;i<=n;++i)
+        {
+            System.out.println(i);
+        }
+    }
+}
