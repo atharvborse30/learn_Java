5aca13cb1dbf634dfc7a571e5456818376e1e218
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-08-22 20:06:14 +0530

    Add files via upload

---
+++ for_each_loop.java
@@ -0,0 +1,11 @@
+class for_each_loop {
+    //for each loop is used to iterate through elements of arrays and collections.
+    public static void main(String[] args){
+        int[] numbers = {1,3,5,7};
+
+        //for each loop
+        for (int number:numbers){
+            System.out.println(number);
+        }
+    }
+}
