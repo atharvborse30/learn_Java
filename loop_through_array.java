66afb1163050fe12cef564bbc058c9338e43b4d9
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-08-23 19:29:47 +0530

    Add files via upload

---
+++ loop_through_array.java
@@ -0,0 +1,12 @@
+class loop_through_array{
+    public static void main(String[] args){
+        //create an array
+        int[] age = {12,4,5};
+
+        //loop through the array
+        System.out.println("Using for loop: ");
+        for(int i=0;i<age.length;i++){
+            System.out.println(age[i]);
+        }
+    }
+}
