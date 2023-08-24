cf65e926c3aa870e36b05963beaac7e1670c364e
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-08-24 22:49:14 +0530

    Add files via upload

---
+++ create_string_using_new_keyword.java
@@ -0,0 +1,8 @@
+class create_string_using_new_keyword {
+    public static void main(String[] args){
+        //create a string using new
+        String name = new String("Java String");
+
+        System.out.println(name);
+    }
+}
