a9df27a11ea677eba01453e5087b48ee411d90fd
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-08-24 21:20:08 +0530

    Add files via upload

---
+++ standard_library_method.java
@@ -0,0 +1,6 @@
+class standard_library_method {
+    public static void main(String[] args) {
+        //using the sqrt() method
+        System.out.println("Square root of 4 is : "+Math.sqrt(4));
+    }
+}
