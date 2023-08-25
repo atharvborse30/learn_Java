77e799181b628dbd8c8c906416bc8c837acbf074
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-08-25 22:28:04 +0530

    Add files via upload

---
+++ final_variable.java
@@ -0,0 +1,9 @@
+class final_variable{
+    public static void main(String[] args){
+        // create a final variable
+        final int AGE = 32;
+
+        //try to change the final variable
+        System.out.println("Age " + AGE);
+    }
+}
