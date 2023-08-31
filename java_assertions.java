65969d385a0494dcc1e94923a965e0fa4bf6f2a1
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-08-31 22:19:37 +0530

    Add files via upload

---
+++ java_assertions.java
@@ -0,0 +1,7 @@
+public class java_assertions {
+    public static void main(String[] args) {
+        String[] weekends = {"Friday","Saturday","Sunday"};
+        assert weekends.length == 2;
+        System.out.println("There are "+weekends.length + " weekends in a week");
+    }
+}
