e76d48eb420d6beb27faa635a0f0d5f899c7e808
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-08-31 22:09:16 +0530

    Add files via upload

---
+++ multi_catch.java
@@ -0,0 +1,10 @@
+public class multi_catch {
+    public static void main(String[] args) {
+        try {
+          int array[] = new int[10];
+          array[10] = 30 / 0;
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+          System.out.println(e.getMessage());
+        }
+      }
+}
