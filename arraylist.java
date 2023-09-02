61a28765fa3cfe36685b7e9721251c9f976b388c
Atharv Borse <61404222+atharvborse30@users.noreply.github.com>
2023-09-02 20:50:14 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class arraylist {
+    public static void main(String[] args){
+        ArrayList<String> animals = new ArrayList<>();
+        //add elements
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+        System.out.println("ArrayList: "+animals);
+    }
+}
